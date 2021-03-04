11ad325d722b9b83b7a6c4aa83d5f12813c778b3
Adi Sumardi <adisumardi1996@gmail.com>
2021-03-04 10:24:52 +0700

    initial commit

---
+++ resources/views/create-category.blade.php
@@ -0,0 +1,17 @@
+@extends('template.template')
+
+@section('style')
+  <link rel="stylesheet" href="{{asset('style/style.css')}}">  
+@endsection
+<link rel="stylesheet" href="{{asset('style/style.css')}}">
+@section('content')
+  <form class="mb-4" action="/category-store" method="POST">
+    @csrf
+    <h1 class="text-center mb-4">Create Category</h1>
+    <div class="form-group">
+        <label for="">Category Name</label>
+        <input type="text" class="form-control" name="name">
+    </div>
+    <button id="btn-submit" class="btn btn-primary mt-3">Submit</button>
+  </form>
+@endsection
